510c822ff1c1a9b42bfac90630a2627ed1bc6c94
Thar Htoo Aung <tharhtooaung811184@gmail.com>
2023-06-08 11:24:15 +0630

    updated tutorial2

--- src/main/java/tutorial/Tutorial2b.java
+++ src/main/java/tutorial/Tutorial2b.java
@@ -1,5 +1,27 @@
 package main.java.tutorial;
 
-public class Tutorial2b {
+import java.util.Scanner;
 
+public class Tutorial2b {
+	public static void main(String[] args) {
+		int largestNum = Integer.MIN_VALUE;
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Input 1st number: ");
+		int firstNum = scanner.nextInt();
+		if (firstNum >= largestNum) {
+			largestNum = firstNum;
+		}
+		System.out.print("Input 2nd number: ");
+		int secondNum = scanner.nextInt();
+		if (secondNum >= largestNum) {
+			largestNum = secondNum;
+		}
+		System.out.print("Input 3rd number: ");
+		int thirdNum = scanner.nextInt();
+		if (thirdNum >= largestNum) {
+			largestNum = thirdNum;
+		}
+		System.out.println("Expected Output :");
+		System.out.print("The greatest: " + largestNum);
+	}
 }
